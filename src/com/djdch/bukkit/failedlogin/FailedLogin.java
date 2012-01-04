e698ca601a148b42714ee673dc480af33dd68bd5
DjDCH <bukkit.coding@djdch.com>
2012-01-04 18:53:09 -0500

    Fixed files for compliance of coding conventions.

--- src/com/djdch/bukkit/failedlogin/FailedLogin.java
+++ src/com/djdch/bukkit/failedlogin/FailedLogin.java
@@ -11,46 +11,46 @@ import com.djdch.bukkit.utils.Logger;
  * Main class of the <b>FailedLogin</b> plugin for Bukkit.
  * <p>
  * Send custom kick message when the login process failed.
- *
+ * 
  * @author DjDCH
  */
 public class FailedLogin extends JavaPlugin {
-	/**
-	 * Contains the Logger instance.
-	 */
-	protected final Logger logger = new Logger();
-	
-	/**
-	 * Contains the loginListener instance.
-	 */
-	protected final LoginListener loginListener = new LoginListener(this);
-	
-	/**
-	 * Method execute when the plugin is enable.
-	 */
-	public void onEnable() {
-		this.logger.setPrefix(getDescription().getName());
-		
-		// Register the plugin events
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvent(Event.Type.PLAYER_LOGIN, loginListener, Event.Priority.Normal, this);
-		
-		logger.info("Version " + getDescription().getVersion() + " enable.");
-	}
-	
-	/**
-	 * Method execute when the plugin is disable.
-	 */
-	public void onDisable() {
-		logger.info("Version " + getDescription().getVersion() + " disable.");
-	}
-	
-	/**
-	 * Accessor who return the logger instance.
-	 * 
-	 * @return Logger instance.
-	 */
-	public Logger getLogger() {
-		return this.logger;
-	}
+    /**
+     * Contains the Logger instance.
+     */
+    protected final Logger logger = new Logger();
+
+    /**
+     * Contains the loginListener instance.
+     */
+    protected final LoginListener loginListener = new LoginListener(this);
+
+    /**
+     * Method execute when the plugin is enable.
+     */
+    public void onEnable() {
+        this.logger.setPrefix(getDescription().getName());
+
+        // Register the plugin events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_LOGIN, loginListener, Event.Priority.Normal, this);
+
+        logger.info("Version " + getDescription().getVersion() + " enable.");
+    }
+
+    /**
+     * Method execute when the plugin is disable.
+     */
+    public void onDisable() {
+        logger.info("Version " + getDescription().getVersion() + " disable.");
+    }
+
+    /**
+     * Accessor who return the logger instance.
+     * 
+     * @return Logger instance.
+     */
+    public Logger getLogger() {
+        return this.logger;
+    }
 }
--- src/com/djdch/bukkit/failedlogin/listeners/LoginListener.java
+++ src/com/djdch/bukkit/failedlogin/listeners/LoginListener.java
@@ -12,30 +12,30 @@ import com.djdch.bukkit.failedlogin.FailedLogin;
  * @author DjDCH
  */
 public class LoginListener extends PlayerListener {
-	/**
-	 * Contains the FailedLogin instance.
-	 */
-	protected FailedLogin failedLogin;
-	
-	/**
-	 * Constructor for the initialization of the LoginListener class.
-	 * 
-	 * @param failedLogin Contains the FailedLogin instance.
-	 */
-	public LoginListener(FailedLogin failedLogin) {
-		this.failedLogin = failedLogin;
-	}
-	
-	/**
-	 * Method who is called each time a player tries to login.
-	 * 
-	 * @param event Contains the PlayerLoginEvent instance.
-	 */
-	public void onPlayerLogin(PlayerLoginEvent event) {
-		if (event.getResult() == Result.KICK_BANNED) {
-			event.setKickMessage("You haven't paid for this month. Please proceed with the payment or contact the administrator.");
-		} else if (event.getResult() == Result.KICK_WHITELIST) {
-			event.setKickMessage("You are not white-listed on this server. Please contact the administrator.");
-		}
-	}
+    /**
+     * Contains the FailedLogin instance.
+     */
+    protected FailedLogin failedLogin;
+
+    /**
+     * Constructor for the initialization of the LoginListener class.
+     * 
+     * @param failedLogin Contains the FailedLogin instance.
+     */
+    public LoginListener(FailedLogin failedLogin) {
+        this.failedLogin = failedLogin;
+    }
+
+    /**
+     * Method who is called each time a player tries to login.
+     * 
+     * @param event Contains the PlayerLoginEvent instance.
+     */
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        if (event.getResult() == Result.KICK_BANNED) {
+            event.setKickMessage("You haven't paid for this month. Please proceed with the payment or contact the administrator.");
+        } else if (event.getResult() == Result.KICK_WHITELIST) {
+            event.setKickMessage("You are not white-listed on this server. Please contact the administrator.");
+        }
+    }
 }
