553f7f074f47caf5f8b72ee5b72989f8fe6125c9
DjDCH <bukkit.coding@djdch.com>
2012-01-04 19:08:49 -0500

    Added ConfigurationException class.

---
+++ src/com/djdch/bukkit/failedlogin/configuration/ConfigurationException.java
@@ -0,0 +1,19 @@
+package com.djdch.bukkit.failedlogin.configuration;
+
+/**
+ * Class who extends from the Exception class for exception occurring in the ConfigManager.
+ * 
+ * @author DjDCH
+ */
+public class ConfigurationException extends Exception {
+    /**
+     * Contains the UID of this exception.
+     */
+    private static final long serialVersionUID = 1794161910300777417L;
+
+    /**
+     * Constructor of the ConfigException.
+     */
+    public ConfigurationException() {
+    }
+}
